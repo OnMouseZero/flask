4dec090a17fd1f94c5035f56f22d3d16c75f554b
onmouse <onmouse@163.com>
2021-01-18 21:37:50 +0800

    6.0版本提交

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! this is v5.0版本的flask"
+    return "Hello World! this is v6.0版本的flask"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
